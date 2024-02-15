db2103c1240968795746c9453dbc152807ef1b29
Satvik Gaikwad <satvik.g.code@gmail.com>
2024-02-15 19:49:51 +0530

    LeetCode\1239. Maximum Length of a Concatenated String with Unique Characters

---
+++ LeetCode/1239. Maximum Length of a Concatenated String with Unique Characters/main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+  int maxLength(vector<string> &arr)
+  {
+    
+  }
+};
+
+int main()
+{
+  vector<string> arr;
+  string s;
+  while (cin >> s)
+  {
+    arr.push_back(s);
+  }
+  Solution solution;
+  cout << solution.maxLength(arr) << endl;
+  return 0;
+}
